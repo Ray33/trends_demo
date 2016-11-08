66829296b5231f0e1c53a77f447bea0044646d24
Viacheslav Titov <viacheslavtitov90@gmail.com>
2016-11-08 11:32:31 +0200

    re #1637 - delete trending keyword

--- app/src/main/java/io/mobitech/trends/demo/api/ApiManager.java
+++ app/src/main/java/io/mobitech/trends/demo/api/ApiManager.java
@@ -9,12 +9,15 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.List;
 
 import io.mobitech.trends.demo.BuildConfig;
+import io.mobitech.trends.demo.TrendsDemoApplication;
 
 /**
  * Created by Viacheslav Titov on 07.11.2016.
@@ -25,27 +28,32 @@ public class ApiManager {
     private static final String LOG = ApiManager.class.getPackage() + "." + ApiManager.class.getSimpleName();
 
     private static final String USER_AGENT = "Mozilla/5.0";
+    private static final String ACCEPT_LANGUAGE = "en-US,en;q=0.5";
 
     private static final String URL_BASE = "http://trends-vertx.mobitech-search.xyz/";
-    private static final String URL_TRENDING_LIST = "mobitech/trends";
+    private static final String URL_TRENDING_LIST = "mobitech/trends/?p_key=%s&user_id=%s&p_id=%s&c=%s";
+    private static final String URL_TRENDING_DELETE = "mobitech/trends/delete/?p_key=%s&user_id=%s&p_id=%s&keywords=%s";
 
-    private static String getTrendsUrl() {
-        return URL_BASE + URL_TRENDING_LIST;
+    private static String getTrendsUrl(String country) {
+        String url = URL_BASE + URL_TRENDING_LIST;
+        return String.format(url, TrendsDemoApplication.getInstance().getApiKey(),
+                TrendsDemoApplication.getInstance().getUserId(),
+                "search_sdk", TextUtils.isEmpty(country) ? "US" : country);
     }
 
-    public static List<String> getTrendingKeywords(String publishKey, String userId, String country) {
+    private static String getTrendDeleteUrl(String keywords) throws UnsupportedEncodingException {
+        String url =  URL_BASE + URL_TRENDING_DELETE;
+        return String.format(url, TrendsDemoApplication.getInstance().getApiKey(),
+                TrendsDemoApplication.getInstance().getUserId(), "search_sdk",
+                URLEncoder.encode(keywords, "UTF-8"));
+    }
+
+    public static List<String> getTrendingKeywords(String country) {
         Log.d(LOG, "------- getTrendingKeywords -------");
         List<String> result = new ArrayList<>();
         try {
-            HttpURLConnection connection = (HttpURLConnection) new URL(getTrendsUrl()).openConnection();
-            connection.setRequestMethod("GET");
-
-            connection.setRequestProperty("User-Agent", USER_AGENT);
-            connection.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
-            connection.addRequestProperty("p_key", publishKey);
-            connection.addRequestProperty("user_id", userId);
-            connection.addRequestProperty("p_id", "search_sdk");
-            connection.addRequestProperty("c", TextUtils.isEmpty(country) ? "US" : country);
+            HttpURLConnection connection = prepareHttpGetConnection(getTrendsUrl(country));
+            Log.d(LOG, connection.getURL().toString());
 
             final int responseCode = connection.getResponseCode();
             Log.d(LOG, "response code: " + responseCode);
@@ -61,12 +69,44 @@ public class ApiManager {
                 Log.d(LOG, responseBody);
             }
             result = JsonParserManager.getTrendingKeywords(responseBody);
-        } catch (IOException e) {
-            Log.e(LOG, e.getMessage());
-        } catch (JSONException e) {
+        } catch (IOException | JSONException e) {
             Log.e(LOG, e.getMessage());
         }
         return result;
+    }
+
+    public static boolean deleteTrendingKeyword(String keywords) {
+        Log.d(LOG, "------- deleteTrendingKeyword -------");
+        try {
+            HttpURLConnection connection = prepareHttpGetConnection(getTrendDeleteUrl(keywords));
+            Log.d(LOG, connection.getURL().toString());
+
+            final int responseCode = connection.getResponseCode();
+            Log.d(LOG, "response code: " + responseCode);
+            String responseBody;
+            if (200 <= responseCode && responseCode <= 299) {
+                responseBody = getStringFromInputStream(connection.getInputStream());
+            } else {
+                responseBody = getStringFromInputStream(connection.getErrorStream());
+                Log.e(LOG, responseBody);
+                return false;
+            }
+            if (BuildConfig.DEBUG) {
+                Log.d(LOG, responseBody);
+            }
+        } catch (IOException e) {
+            Log.e(LOG, e.getMessage());
+            return false;
+        }
+        return true;
+    }
+
+    private static HttpURLConnection prepareHttpGetConnection(String url) throws IOException {
+        HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
+        connection.setRequestMethod("GET");
+        connection.setRequestProperty("User-Agent", USER_AGENT);
+        connection.setRequestProperty("Accept-Language", ACCEPT_LANGUAGE);
+        return connection;
     }
 
     private static String getStringFromInputStream(InputStream is) throws IOException {
--- app/src/main/java/io/mobitech/trends/demo/ui/activity/MainActivity.java
+++ app/src/main/java/io/mobitech/trends/demo/ui/activity/MainActivity.java
@@ -1,7 +1,9 @@
 package io.mobitech.trends.demo.ui.activity;
 
+import android.content.DialogInterface;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -9,7 +11,6 @@ import android.support.v7.widget.RecyclerView;
 import java.util.List;
 
 import io.mobitech.trends.demo.R;
-import io.mobitech.trends.demo.TrendsDemoApplication;
 import io.mobitech.trends.demo.api.ApiManager;
 import io.mobitech.trends.demo.ui.adapters.TrendsRecyclerViewAdapter;
 import io.mobitech.trends.demo.ui.interfaces.OnTrendsClickListener;
@@ -42,8 +43,7 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             protected List<String> doInBackground(Void... voids) {
-                return ApiManager.getTrendingKeywords(TrendsDemoApplication.getInstance().getApiKey(),
-                        TrendsDemoApplication.getInstance().getUserId(), null);
+                return ApiManager.getTrendingKeywords(null);
             }
 
             @Override
@@ -63,10 +63,43 @@ public class MainActivity extends AppCompatActivity {
         }, new OnTrendsDeleteClickListener() {
             @Override
             public void onTrendsDelete(String trendsKeyword) {
-
+                showDialogDeleteTrendingKeyword(trendsKeyword);
             }
         });
         mRecyclerView.setAdapter(adapter);
+    }
+
+    private void showDialogDeleteTrendingKeyword(final String trendsKeyword) {
+        AlertDialog.Builder alertBuilder = new AlertDialog.Builder(this);
+        alertBuilder.setTitle(R.string.confirmation);
+        String message = String.format(getString(R.string.dialog_delete_trends_keywords), trendsKeyword);
+        alertBuilder.setMessage(message);
+        alertBuilder.setNegativeButton(R.string.no, null);
+        alertBuilder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                deleteTrendingKeyword(trendsKeyword);
+            }
+        });
+        alertBuilder.create().show();
+    }
+
+    private void deleteTrendingKeyword(final String trendsKeyword) {
+        AsyncTask<Void, Void, Boolean> task = new AsyncTask<Void, Void, Boolean>() {
+
+            @Override
+            protected Boolean doInBackground(Void... voids) {
+                return ApiManager.deleteTrendingKeyword(trendsKeyword);
+            }
+
+            @Override
+            protected void onPostExecute(Boolean result) {
+                if (result) {
+                    updateData();
+                }
+            }
+        };
+        task.execute();
     }
 
 }
